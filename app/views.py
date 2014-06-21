32a68267e5e4baa37711c427a602665c943626bf
Wyatt Carss <carss.w@gmail.com>
2014-06-21 12:12:23 -0400

    Add bare templates for index, view, and make.

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/make')
 def make():
-    return 'Make!'
+    return render_template('make.html')
 
 @app.route('/view')
 def view():
-    return 'View!'
+    return render_template('view.html')
