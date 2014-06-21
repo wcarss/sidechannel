3dbe10d7bcc8cfcfe7667ca19f9b2888dbfdb507
Wyatt Carss <carss.w@gmail.com>
2014-06-21 19:47:25 -0400

    Extract joke generation from view.
    This was a big ol' pile of mess, and it still is pretty much that.
    
    The mess is just a teensy bit smaller, now.

--- app/views.py
+++ app/views.py
@@ -18,51 +18,55 @@ def index():
 def make():
     return render_template('make.html')
 
-previous_first_part = "AND THEN"
-previous_second_part = "WE ALL WENT QUIET"
-last_background = 'static/all_went_quiet.gif'
+last_joke = {
+  'top': "AND THEN",
+  'bottom': "WE ALL WENT QUIET",
+  'image_url': 'static/all_went_quiet.gif'
+}
 last_time = datetime.datetime.now()
 
 @app.route('/')
 @app.route('/view')
 def view():
-    global previous_first_part
-    global previous_second_part
-    global last_background
+    global last_joke
     global last_time
-    global redis_client
 
     time = datetime.datetime.now()
     if time - last_time > datetime.timedelta(minutes=3):
         last_time = time
-        sentence = x11r5.get_quote(length=10)
-        words = sentence.split(" ")
-        num_words = len(words)
-        previous_first_part = " ".join(words[0:num_words/2])
-        previous_second_part = " ".join(words[num_words/2+1:])
-        last_background=giphy.GiphyAPI.get_random_image_url(random.choice(words))
-        identifier = redis_client.incr("joke_id")
-        joke = {
-           'id': identifier,
-           'top': previous_first_part,
-           'bottom': previous_second_part,
-           'image_url': last_background,
-           'time': str(last_time),
-           'channel': "AngelHackTO"
-        }
-        redis_client.hmset("joke:%s" % identifier, joke)
+        last_joke = generate_joke(last_time)
 
-    background = last_background
-    first_part = html_parser.unescape(previous_first_part)
-    second_part = html_parser.unescape(previous_second_part)
+    joke = last_joke
 
     return render_template('view.html',
-        first_part=first_part,
-        second_part=second_part,
-        background=background)
+        first_part=html_parser.unescape(joke['top']),
+        second_part=html_parser.unescape(joke['bottom']),
+        background=joke['image_url'])
 
 @app.route('/reset')
 def reset():
     global last_time
     last_time = last_time - datetime.timedelta(minutes=4)
     return "okay!"
+
+def generate_joke(time):
+    global redis_client
+    sentence = x11r5.get_quote(length=10)
+    words = sentence.split(" ")
+    num_words = len(words)
+    top = " ".join(words[0:num_words/2])
+    bottom = " ".join(words[num_words/2+1:])
+    image_url = giphy.GiphyAPI.get_random_image_url(random.choice(words))
+
+    identifier = redis_client.incr("joke_id")
+    joke = {
+       'id': identifier,
+       'top': top,
+       'bottom': bottom,
+       'image_url': image_url,
+       'time': str(time),
+       'channel': "AngelHackTO"
+    }
+    redis_client.hmset("joke:%s" % identifier, joke)
+
+    return joke
