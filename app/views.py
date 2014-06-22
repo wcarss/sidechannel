b987d7792ec4604e9f3aca5a5a2ddb0636f5516d
Wyatt Carss <carss.w@gmail.com>
2014-06-21 20:23:08 -0400

    Refactoring to kill globals.
    Things have to be moved around here so that 'save_joke' exists at
    the point that it is being called. Everything works here, so this
    is getting committed before we move on.
    
    This is part of a series of commits aimed at making views
    responsible for a minimal amount of logic, and making jokes easy
    to retrieve by identifier.

--- app/views.py
+++ app/views.py
@@ -10,45 +10,10 @@ html_parser = HTMLParser.HTMLParser()
 redis_client = redis.Redis("localhost")
 redis_client.set("joke_id", 0)
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-@app.route('/make')
-def make():
-    return render_template('make.html')
-
-last_joke = {
-  'top': "AND THEN",
-  'bottom': "WE ALL WENT QUIET",
-  'image_url': 'static/all_went_quiet.gif'
-}
-last_time = datetime.datetime.now()
-
-@app.route('/')
-@app.route('/view')
-def view():
-    global last_joke
-    global last_time
-
-    time = datetime.datetime.now()
-    if time - last_time > datetime.timedelta(minutes=3):
-        last_time = time
-        (top, bottom, image_url) = generate_random_joke_pieces()
-        last_joke = save_joke(top, bottom, image_url, time)
-
-    joke = last_joke
-
-    return render_template('view.html',
-        first_part=html_parser.unescape(joke['top']),
-        second_part=html_parser.unescape(joke['bottom']),
-        background=joke['image_url'])
-
-@app.route('/reset')
-def reset():
-    global last_time
-    last_time = last_time - datetime.timedelta(minutes=4)
-    return "okay!"
+def new_joke(time):
+    (top, bottom, image_url) = generate_random_joke_pieces()
+    joke = save_joke(top, bottom, image_url, time)
+    return joke
 
 def generate_random_joke_pieces():
     sentence = x11r5.get_quote(length=10)
@@ -71,5 +36,51 @@ def save_joke(top, bottom, image_url, time):
        'channel': "AngelHackTO"
     }
     redis_client.hmset("joke:%s" % identifier, joke)
-
     return joke
+
+def get_latest_joke():
+    global redis_client
+    identifier = redis_client.get("joke_id")
+    joke = redis_client.hgetall("joke:%s" % identifier)
+    return joke
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/make')
+def make():
+    return render_template('make.html')
+
+last_time = datetime.datetime.now()
+last_joke = save_joke(
+    "AND THEN",
+    "WE ALL WENT QUIET",
+    "static/all_went_quiet.gif",
+    last_time)
+
+@app.route('/')
+@app.route('/view')
+def view():
+    global last_time
+
+    time = datetime.datetime.now()
+    if time - last_time > datetime.timedelta(minutes=3):
+        last_time = time
+        joke = new_joke(last_time)
+    else:
+        joke = get_latest_joke()
+
+    #print joke
+    #print type(joke)
+
+    return render_template('view.html',
+        first_part=html_parser.unescape(joke['top']),
+        second_part=html_parser.unescape(joke['bottom']),
+        background=joke['image_url'])
+
+@app.route('/reset')
+def reset():
+    global last_time
+    last_time = last_time - datetime.timedelta(minutes=4)
+    return "okay!"
