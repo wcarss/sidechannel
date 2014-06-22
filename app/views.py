e61950e4e50b108659623a8eaedf3b670b929053
Wyatt Carss <carss.w@gmail.com>
2014-06-21 21:00:28 -0400

    Kill the last global variable.
    Jokes are now retained as long as the server is up.
    
    Resets now just straight-up generate a new joke, instead of making
    the "last_time" be further back in time than it really was -- which
    has the wonderful benefit of putting the reset load on me as the
    admin, instead of on the next viewer to hit the page after I
    trigger a reset.
    
    The time logic is also pretty isolated now, and might get broken
    into a utility function.

--- app/views.py
+++ app/views.py
@@ -8,11 +8,10 @@ import redis
 
 html_parser = HTMLParser.HTMLParser()
 redis_client = redis.Redis("localhost")
-redis_client.set("joke_id", 0)
 
-def new_joke(time):
+def new_joke():
     (top, bottom, image_url) = generate_random_joke_pieces()
-    joke = save_joke(top, bottom, image_url, time)
+    joke = save_joke(top, bottom, image_url)
     return joke
 
 def generate_random_joke_pieces():
@@ -24,7 +23,7 @@ def generate_random_joke_pieces():
     image_url = giphy.GiphyAPI.get_random_image_url(random.choice(words))
     return (top, bottom, image_url)
 
-def save_joke(top, bottom, image_url, time):
+def save_joke(top, bottom, image_url):
     global redis_client
     identifier = redis_client.incr("joke_id")
     joke = {
@@ -32,7 +31,7 @@ def save_joke(top, bottom, image_url, time):
        'top': top,
        'bottom': bottom,
        'image_url': image_url,
-       'time': str(time),
+       'time': str(datetime.datetime.now()),
        'channel': "AngelHackTO"
     }
     redis_client.hmset("joke:%s" % identifier, joke)
@@ -41,7 +40,10 @@ def save_joke(top, bottom, image_url, time):
 def get_latest_joke():
     global redis_client
     identifier = redis_client.get("joke_id")
-    joke = redis_client.hgetall("joke:%s" % identifier)
+    if identifier:
+        joke = redis_client.hgetall("joke:%s" % identifier)
+    else:
+        joke = None
     return joke
 
 @app.route('/index')
@@ -52,27 +54,17 @@ def index():
 def make():
     return render_template('make.html')
 
-last_time = datetime.datetime.now()
-last_joke = save_joke(
-    "AND THEN",
-    "WE ALL WENT QUIET",
-    "static/all_went_quiet.gif",
-    last_time)
-
 @app.route('/')
 @app.route('/view')
 def view():
-    global last_time
-
+    joke = get_latest_joke()
     time = datetime.datetime.now()
-    if time - last_time > datetime.timedelta(minutes=3):
-        last_time = time
-        joke = new_joke(last_time)
-    else:
-        joke = get_latest_joke()
-
-    #print joke
-    #print type(joke)
+    print joke
+    print type(joke)
+    # dates like "2014-06-21 20:38:15.191355"
+    joke_time = datetime.datetime.strptime(joke['time'], "%Y-%m-%d %H:%M:%S.%f")
+    if joke is None or time - joke_time > datetime.timedelta(minutes=3):
+        joke = new_joke()
 
     return render_template('view.html',
         first_part=html_parser.unescape(joke['top']),
@@ -81,6 +73,5 @@ def view():
 
 @app.route('/reset')
 def reset():
-    global last_time
-    last_time = last_time - datetime.timedelta(minutes=4)
+    new_joke()
     return "okay!"
